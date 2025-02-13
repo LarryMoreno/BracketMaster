033fbdc55db45e3d76c3bece2085bdbc23d939a1
Saaquib Bokhari <saaquibhbokhari@gmail.com>
2025-02-13 16:47:12 -0500

    added some documentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 // import TournamentPage from "./pages/TournamentPage";
-// TODO: implement page foe tournaments
+// TODO: implement page for tournaments
 
+// render the pages
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/tournament/:id" element={<TournamentPage />} /> */}
+        {/* add this back when tournmanent page is done <Route path="/tournament/:id" element={<TournamentPage />} /> */}
       </Routes>
     </Router>
   );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 // TODO: fix styling w/ tailwind css
 
+//Home page for bracket master
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
