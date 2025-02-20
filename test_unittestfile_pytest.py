41dc0f12854004a9bc4ec40b312e57eb21b48407
Collin-Heaney <128501976+Collin-Heaney@users.noreply.github.com>
2025-02-20 00:03:18 -0500

    test_account_success and test_account_username_exists

--- test_unittestfile_pytest.py
+++ test_unittestfile_pytest.py
@@ -6,10 +6,11 @@ def client():
     with app.test_client() as client:
         yield client
 
+#testing that an account can be created on the system
 def test_account_registration_success(client):
     json_data = {
-        "userID": "004",
-        "username" : "username",
+        "userID": "007",
+        "username" : "newnewuser",
         "password" : "password",
         "email" : "email@email.com",
         "role" : "member"
@@ -19,3 +20,18 @@ def test_account_registration_success(client):
 
     assert response.status_code == 200
     assert response.json['message'] == 'User registered successfully'
+
+#testing that an account cannot be created where a username already exists
+def test_account_creation_username_exists(client):
+    json_data = {
+        "userID": "007",
+        "username" : "newnewuser",
+        "password" : "password",
+        "email" : "email@email.com",
+        "role" : "member"
+    }
+
+    response = client.post('/register', json=json_data)
+
+    assert response.status_code == 409
+    assert response.json['error'] == 'Username already exists'
