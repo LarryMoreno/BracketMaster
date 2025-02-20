6770adbb07623ae7dea5d864dc9d05b384fbfb59
Collin-Heaney <128501976+Collin-Heaney@users.noreply.github.com>
2025-02-20 12:18:29 -0500

    Update test_unittestfile_pytest.py

--- test_unittestfile_pytest.py
+++ test_unittestfile_pytest.py
@@ -9,11 +9,9 @@ def client():
 #testing that an account can be created on the system
 def test_account_registration_success(client):
     json_data = {
-        "userID": "007",
-        "username" : "newnewuser",
+        "username" : "goat",
         "password" : "password",
         "email" : "email@email.com",
-        "role" : "member"
     }
 
     response = client.post('/register', json=json_data)
@@ -22,13 +20,11 @@ def test_account_registration_success(client):
     assert response.json['message'] == 'User registered successfully'
 
 #testing that an account cannot be created where a username already exists
-def test_account_creation_username_exists(client):
+def test_account_registration_username_exists(client):
     json_data = {
-        "userID": "007",
         "username" : "newnewuser",
         "password" : "password",
         "email" : "email@email.com",
-        "role" : "member"
     }
 
     response = client.post('/register', json=json_data)
@@ -39,15 +35,12 @@ def test_account_creation_username_exists(client):
 #testing that an account cannot be created where the username has 16 or more characters
 def test_account_registration_username_too_long(client):
     json_data = {
-        "userID": "010",
         "username" : "abcdefghijklmnopq",
         "password" : "password",
         "email" : "email@email.com",
-        "role" : "member"
     }
 
     response = client.post('/register', json=json_data)
 
     assert response.status_code == 410
     assert response.json['error'] == 'Username is too many characters'
-
